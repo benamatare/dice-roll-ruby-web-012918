4b7a1363b2a5fac548461ed3ad01c2218943028d
Ben Matare <ben.a.matare@gmail.com>
2018-01-16 02:51:21 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,15 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+
+# How to do the same thing but using an Array instead - All I did was create a local array
+# to the method that gets pushed a random number from 1 - 6 and returns that random number
+# def thingy
+#  ary = []
+#  return ary.push(rand(1..6))
+# end
+
+
 def roll
-  # code goes here
+  return rand(1..6)
 end
